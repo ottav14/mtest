93fc25c4baf7ce5f932ba51618fa2b57f036a7c1
ottav14 <dominicjottaviano@gmail.com>
2025-05-09 17:12:22 -0400

    Refactored rendering code.

--- public/render.js
+++ public/render.js
@@ -1,46 +1,46 @@
-import { noteHeadSize, lineThickness, stemThickness, stemHeight } from './params.js';
-import { toScreen, indexToNote } from './util.js';
+import * as PARAMS from './params.js';
+import * as UTIL from './util.js';
 
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
-export const drawStaff = () => {
-	ctx.beginPath();
+const drawLine = (x1, y1, x2, y2) => {
 	ctx.strokeStyle = '#000';
-	ctx.lineWidth = lineThickness;
-	const y0 = canvas.width/2 - 4*(noteHeadSize+lineThickness);
+	ctx.beginPath();
+	ctx.moveTo(x1, y1);
+	ctx.lineTo(x2, y2);
+	ctx.stroke();
+}
+
+export const drawStaff = () => {
+	const y0 = canvas.width/2 - 4*(PARAMS.noteHeadSize+PARAMS.lineThickness);
 	for(let i=0; i<5; i++) {
-		const y = 2*i*(noteHeadSize+lineThickness) + y0;
-		ctx.moveTo(0, y);
-		ctx.lineTo(canvas.width, y);
-		ctx.stroke();
+		const y = 2*i*(PARAMS.noteHeadSize+PARAMS.lineThickness) + y0;
+		ctx.strokeStyle = '#000';
+		ctx.lineWidth = PARAMS.lineThickness;
+		drawLine(0, y, canvas.width, y);
 	}
 }
 
 export const drawNoteHead = (y) => {
-	const screenY = toScreen(y);
+	const screenY = UTIL.toScreen(y);
 	ctx.beginPath();
-	ctx.ellipse(canvas.width/2, screenY, noteHeadSize, 3 * noteHeadSize / 2, 2 * Math.PI / 6, 0, Math.PI * 2);
+	ctx.ellipse(canvas.width/2, screenY, PARAMS.noteHeadSize, 3 * PARAMS.noteHeadSize / 2, 2 * Math.PI / 6, 0, Math.PI * 2);
 	ctx.fillStyle = '#000';
 	ctx.fill();
 }
 
 export const drawNote = (y) => {
-	const screenY = toScreen(y)-3;
-	const screenX = canvas.width/2 + noteHeadSize + 2.5;
+	const screenY = UTIL.toScreen(y)-3;
+	const screenX = canvas.width/2 + PARAMS.noteHeadSize + 2.5;
 	drawNoteHead(y);
-	ctx.beginPath();
-	ctx.strokeStyle = '#000';
-	ctx.lineWidth = stemThickness;
-	ctx.moveTo(screenX, screenY);
-	ctx.lineTo(screenX, screenY-stemHeight);
-	ctx.stroke();
+	ctx.lineWidth = PARAMS.stemThickness;
+	drawLine(screenX, screenY, screenX, screenY-PARAMS.stemHeight);
 }
 
 export const drawChord = (notes) => {
-	for(const y of notes) {
+	for(const y of notes) 
 		drawNote(y);
-	}
 }
 
 export const clearCanvas = () => {
